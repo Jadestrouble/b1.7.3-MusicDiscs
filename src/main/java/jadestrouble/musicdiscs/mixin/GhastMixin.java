13190b34162011ac1268353b01d35c32cbdb6c88
Jadedestouble <Jadestrouble@gmail.com>
2023-11-03 09:20:11 -0700

    bugfix

--- src/main/java/jadestrouble/musicdiscs/mixin/GhastMixin.java
+++ src/main/java/jadestrouble/musicdiscs/mixin/GhastMixin.java
@@ -1,19 +1,18 @@
 package jadestrouble.musicdiscs.mixin;
 
 import jadestrouble.musicdiscs.items.Discs;
+import net.minecraft.entity.living.FlyingBase;
 import net.minecraft.entity.monster.Ghast;
-import net.minecraft.item.ItemBase;
+import net.minecraft.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Ghast.class)
-public class GhastMixin{
-    @Inject(method = "getMobDrops", at = @At("RETURN"), cancellable = true)
-    private void addDisc(CallbackInfoReturnable<Integer> cir){
-        cir.setReturnValue(Discs.pigstep_Disc.id);
+public class GhastMixin extends FlyingBase {
+    private GhastMixin(Level arg) {
+        super(arg);
+    }
+    @Override
+    protected void getDrops() {
+        dropItem(Discs.pigstep_Disc.id, 1);
     }
 }
-
-
